ab12a9004135c2de79f7b25baeeed391f0b6210e
Jessica Galarza <jessicaga941@gmail.com>
2021-10-14 20:32:18 -0400

    Created a README.md file using nmp inquirer and node.js

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # README-generator
+https://jessica9410.github.io/README-generator/
+# Description
+A node.js application that uses user input from inquirer to generte a README.md file
+ 
+# Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+-[Credits](#credits)
+- [License](#license)
+
+
+# Installation 
+
+* inquirer
+*fs
+*util
+  
+# Usage
+run in terminal:
+*node index.js
+
+# Credits
+
+https://www.youtube.com/watch?v=2VUQABoFOqw
+https://www.youtube.com/watch?v=cqJQ9wbUQto
+# License
+This software is licensed under MIT license.
+# Questions 
+If you have any questions contact me at jessicaga941@gmail.com 
+Copyright(c) 2021 Jessica Galarza
+
---
+++ index.js
@@ -0,0 +1,66 @@
+// TODO: Include packages needed for this application
+
+const fs= require('fs');
+const util = require("util");
+const inquirer= require("inquirer");
+const generateMarkdown = require("./util/generateMarkdown.js")
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// TODO: Create an array of questions for user input
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: 'input', 
+        name: 'project',
+        message:'what is the name of your project?',
+        },
+    {
+    type: 'input', 
+name: 'description',
+message:'please write a description of your project?',
+},
+{
+    type: "input",
+    name: "installation",
+    message: "Describe the installation process if any: ",
+},
+{
+    type: "input",
+    name: "usage",
+    message: "What is this project usage for?"
+},
+{
+    type: 'linput',
+    name: 'license',
+    message: 'What kind of license should your project have?',
+    choices: ["Apache","BSD 3", "None",
+    "GPL 3.0","ISC","MIT"]
+    },
+    {
+type: 'input', 
+name: 'GitHub', 
+message:'whats is your GitHub username?',
+},
+{
+    type: 'input', 
+name: 'email', 
+message:'whats is your e-mail address?',
+} 
+]);
+}
+// TODO: Create a function to initialize app
+async function init() {
+    try {
+        // Ask user questions and generate responses
+        const answers = await promptUser();
+        const generateContent = generateMarkdown(answers);
+        // Write new README.md to dist directory
+        await writeFileAsync('./dist/README.md', generateContent);
+        console.log('✔️  Successfully wrote to README.md');
+    }   catch(err) {
+        console.log(err);
+    }
+  }
+  
+// Function call to initialize app
+init();
